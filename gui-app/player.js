0e89a6f90da79882ba459b79d541333c8bab852a
Rosen Velikov <rosen.velikov@clouway.com>
2022-04-13 00:35:21 +0300

    night 1

--- gui-app/player.js
+++ gui-app/player.js
@@ -51,47 +51,248 @@ async function readMp3Files() {
 
 readMp3Files().then(mp3files => {
   Amplitude.init({ songs: [initPlaylist("star", mp3files)] });
-  window.addEventListener('keyup', newKeyPressHandler(mp3files), true)
+  conf = {
+    playlist: "star",
+    keyEscape: false,
+    bufEscape: "",
+    keyStar: false,
+    bufStar: ""
+  }
+  window.addEventListener('keyup', newKeyPressHandler(mp3files,conf), true)
 })
 
-function newKeyPressHandler(mp3Files) {
+function newKeyPressHandler(mp3Files,conf) {
   return function (event) {
+    console.log(conf);
     console.log(`You pressed ${event.key}`)
     switch (event.key) {
+      case 'Backspace':
+        break;
       case 'Enter':
-        console.log(Amplitude.getPlayerState())
-        switch (Amplitude.getPlayerState()) {
-          case "playing":
-            Amplitude.pause()
-            console.log("PLAY>PAUSE")
-            break;
-          case "stopped":
-            Amplitude.play()
-            console.log("STOP>PLAY")
-            break;
-          case "paused":
-            Amplitude.play()
-            console.log("PAUSE>PLAY")
-            break;
+        if (conf.keyEscape) {
+
+        } else {
+          console.log(Amplitude.getPlayerState())
+          switch (Amplitude.getPlayerState()) {
+            case "playing":
+              Amplitude.pause()
+              console.log("PLAY>PAUSE")
+              break;
+            case "stopped":
+              Amplitude.play()
+              console.log("STOP>PLAY")
+              break;
+            case "paused":
+              Amplitude.play()
+              console.log("PAUSE>PLAY")
+              break;
+          }
         }
         break;
       case "+":
         document.getElementById("amplitude-volume-up").click()
         break;
-      case "1":
-        playNow(1, 1, files)
+      case "1": //End
+      case "End":
+        var key = "1"
+        if (conf.keyStar) {
+          conf.bufStar = conf.bufStar + key;
+          if (conf.bufStar.length == 2) {
+            conf.keyStar = !conf.keyStar
+            songId = conf.bufStar
+            conf.bufStar = ""
+            playNow("star", songId, mp3Files)
+          }
+        } else {
+          if (conf.keyEscape) {
+            conf.bufEscape = conf.bufEscape + key
+          }
+          playNow(conf.playlist, key, mp3Files)
+        }
+        break;
+      case "2": //ArrowDown
+      case "ArrowDown":
+        var key = "2"
+        if (conf.keyStar) {
+          conf.bufStar = conf.bufStar + key;
+          if (conf.bufStar.length == 2) {
+            conf.keyStar = !conf.keyStar
+            songId = conf.bufStar
+            conf.bufStar = ""
+            playNow("star", songId, mp3Files)
+          }
+        } else {
+          if (conf.keyEscape) {
+            conf.bufEscape = conf.bufEscape + key
+          }
+          playNow(conf.playlist, key, mp3Files)
+        }
+        break;
+      case "3": //PageDown
+      case "PageDown":
+        var key = "3"
+        if (conf.keyStar) {
+          conf.bufStar = conf.bufStar + key;
+          if (conf.bufStar.length == 2) {
+            conf.keyStar = !conf.keyStar
+            songId = conf.bufStar
+            conf.bufStar = ""
+            playNow("star", songId, mp3Files)
+          }
+        } else {
+          if (conf.keyEscape) {
+            conf.bufEscape = conf.bufEscape + key
+          }
+          playNow(conf.playlist, key, mp3Files)
+        }
+        break;
+      case "4": //ArrowLeft
+      case "ArrowLeft":
+        var key = "4"
+        if (conf.keyStar) {
+          conf.bufStar = conf.bufStar + key;
+          if (conf.bufStar.length == 2) {
+            conf.keyStar = !conf.keyStar
+            songId = conf.bufStar
+            conf.bufStar = ""
+            playNow("star", songId, mp3Files)
+          }
+        } else {
+          if (conf.keyEscape) {
+            conf.bufEscape = conf.bufEscape + key
+          }
+          playNow(conf.playlist, key, mp3Files)
+        }
+        break;
+      case "5": //Clear
+      case "Clear":
+        var key = "5"
+        if (conf.keyStar) {
+          conf.bufStar = conf.bufStar + key;
+          if (conf.bufStar.length == 2) {
+            conf.keyStar = !conf.keyStar
+            songId = conf.bufStar
+            conf.bufStar = ""
+            playNow("star", songId, mp3Files)
+          }
+        } else {
+          if (conf.keyEscape) {
+            conf.bufEscape = conf.bufEscape + key
+          }
+          playNow(conf.playlist, key, mp3Files)
+        }
+        break;
+      case "6": //ArrowRight
+      case "ArrowRight":
+        var key = "6"
+        if (conf.keyStar) {
+          conf.bufStar = conf.bufStar + key;
+          if (conf.bufStar.length == 2) {
+            conf.keyStar = !conf.keyStar
+            songId = conf.bufStar
+            conf.bufStar = ""
+            playNow("star", songId, mp3Files)
+          }
+        } else {
+          if (conf.keyEscape) {
+            conf.bufEscape = conf.bufEscape + key
+          }
+          playNow(conf.playlist, key, mp3Files)
+        }
+        break;
+      case "7": //Home
+      case "Home":
+        var key = "7"
+        if (conf.keyStar) {
+          conf.bufStar = conf.bufStar + key;
+          if (conf.bufStar.length == 2) {
+            conf.keyStar = !conf.keyStar
+            songId = conf.bufStar
+            conf.bufStar = ""
+            playNow("star", songId, mp3Files)
+          }
+        } else {
+          if (conf.keyEscape) {
+            conf.bufEscape = conf.bufEscape + key
+          }
+          playNow(conf.playlist, key, mp3Files)
+        }
+        break;
+      case "8": //ArrowUp
+      case "ArrowUp":
+        var key = "8"
+        if (conf.keyStar) {
+          conf.bufStar = conf.bufStar + key;
+          if (conf.bufStar.length == 2) {
+            conf.keyStar = !conf.keyStar
+            songId = conf.bufStar
+            conf.bufStar = ""
+            playNow("star", songId, mp3Files)
+          }
+        } else {
+          if (conf.keyEscape) {
+            conf.bufEscape = conf.bufEscape + key
+          }
+          playNow(conf.playlist, key, mp3Files)
+        }
+        break;
+      case "9": //PageUp
+      case "PageUp":
+        var key = "9"
+        if (conf.keyStar) {
+          conf.bufStar = conf.bufStar + key;
+          if (conf.bufStar.length == 2) {
+            conf.keyStar = !conf.keyStar
+            songId = conf.bufStar
+            conf.bufStar = ""
+            playNow("star", songId, mp3Files)
+          }
+        } else {
+          if (conf.keyEscape) {
+            conf.bufEscape = conf.bufEscape + key
+          }
+          playNow(conf.playlist, key, mp3Files)
+        }
+        break;
+      case "0": //Insert
+      case "Insert":
+        var key = "0"
+        if (conf.keyStar) {
+          conf.bufStar = conf.bufStar + key;
+          if (conf.bufStar.length == 2) {
+            conf.keyStar = !conf.keyStar
+            songId = conf.bufStar
+            conf.bufStar = ""
+            playNow("star", songId, mp3Files)
+          }
+        } else {
+          if (conf.keyEscape) {
+            conf.bufEscape = conf.bufEscape + key
+          }
+          playNow(conf.playlist, key, mp3Files)
+        }
+        break;
+      case "*":
+        conf.keyStar = !conf.keyStar
+        conf.bufStar = ""
+        break;
+      case "Escape":
+        conf.keyEscape = !conf.keyEscape
+        conf.bufStar = ""
         break;
     }
   }
 }
 
 
-function playNow(plist, songId, mySongs) {
-  if (mySongs[`${plist}/${songId}`] === undefined) {
+function playNow(plist, songId, mp3Files) {
+  if (mp3Files[`${plist}:${songId}`] === undefined) {
+    console.log("Song not found");
     return;
   }
-  song = mySongs[`${plist}:${songId}`]
-  song.cover_art_url = mySongs[`${plist}:png`]
+  song = mp3Files[`${plist}:${songId}`]
+  if (plist != "star") {song.cover_art_url = mp3Files[`${plist}:png`]}
+  // song.cover_art_url = mp3Files[`${plist}:png`]
   Amplitude.playNow(song)
 }
 
