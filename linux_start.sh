b9fd0f78cb682bdb4eb0c7b2b1d563b8d2edbc39
Rosen Velikov <rosen.velikov@clouway.com>
2022-06-05 12:08:17 +0300

    desktop settings

--- linux_start.sh
+++ linux_start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-cd gui-app
+gsettings set org.gnome.desktop.screensaver lock-enabled false
+gsettings set org.gnome.desktop.lockdown disable-lock-screen true
+gsettings set org.gnome.desktop.background picture-options 'centered'
+gsettings set org.gnome.desktop.background picture-options 'scaled'
+gsettings set org.gnome.desktop.background picture-uri file:////home/rosen/Desktop/trinavis/gui-app/assets0f8m3quovf/star/bg.bulgaria.png
+cd /home/rosen/Desktop/trinavis/gui-app
 npm start
