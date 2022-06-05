f96ba445f16f0117114fa0b2eef508ea0cc656ff
Rosen Velikov <rosen.velikov@clouway.com>
2022-06-05 12:26:17 +0300

    BG remove

--- linux_start.sh
+++ linux_start.sh
@@ -2,8 +2,5 @@
 git pull
 gsettings set org.gnome.desktop.screensaver lock-enabled false
 gsettings set org.gnome.desktop.lockdown disable-lock-screen true
-gsettings set org.gnome.desktop.background picture-options 'centered'
-gsettings set org.gnome.desktop.background picture-options 'scaled'
-gsettings set org.gnome.desktop.background picture-uri file:////home/rosen/Desktop/trinavis/gui-app/assets0f8m3quovf/star/bg.bulgaria.png
 cd /home/rosen/Desktop/trinavis/gui-app
 npm start
