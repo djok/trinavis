290c214a344d6e6533b3a5bc23f01fd0323b191f
Rosen Velikov <rosen.velikov@clouway.com>
2022-06-09 17:11:40 +0300

    start fix

--- linux_start.sh
+++ linux_start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd /home/rosen/Desktop/trinavis
 git pull
 gsettings set org.gnome.desktop.screensaver lock-enabled false
 gsettings set org.gnome.desktop.lockdown disable-lock-screen true
